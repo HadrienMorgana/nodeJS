a9403cca738db9ac172e4f61d8685f036a8ed679
Tcharlyson <tcharlyson.platel@ynov.com>
2016-10-09 18:31:13 +0200

    Améliorations

--- index.js
+++ index.js
@@ -7,18 +7,38 @@ const Twitter = require('twitter');
 const db = require('sqlite');
 const jade = require('jade');
 const spawn = require('child_process').spawn
+const xlsx = require('xlsx');
+const Promise = require('bluebird');
+const gutil = require('gulp-util');
 
 function start()
 {
 	program
 		.version('1.0.0')
-		.option('-s, --start', 'Start twitter crawler');
-
-	program.parse(process.argv);
+		.option('-s, --start', 'Start twitter crawler')
+		.option('-c, --create [database]', 'Create database to save data')
+		.option('-d, --delete [database]', 'Delete database to erase data')
+		.parse(process.argv);
 
 	if(program.start)
 	{
 		menu();
+	} else if (program.create) {
+		if(program.create.indexOf(".db") > -1){
+			createTable(program.create, "", false);
+		}
+		else {
+			console.log('Veuillez retaper le nom de la base en incluant l\'extention .db');
+		}
+	} else if (program.delete) {
+		fs.exists('./'+program.delete, function(exists) {
+		  if(exists) {
+		    console.log(gutil.colors.green('File exists. Deleting now ...'));
+		    fs.unlink('./'+program.delete);
+		  } else {
+		    console.log(gutil.colors.red('File not found, so not deleting.'));
+		  }
+		});
 	} else {
 		program.help();
 	}
@@ -40,53 +60,29 @@ function menu()
 	]).then((answer) => {
 		if(answer.choice == "Enregister les données en base")
 		{
-			chooseDatabase();
+			askQuestion("Dans quelle base voulez-vous sauvegarder les données ?").then((save)=>{
+							inquirer.prompt([
+		 			 		{
+		 			 			type: 'input',
+		 			 			message: 'Tapez le hashtag à rechercher',
+		 			 			name: 'hashtag',
+		 			 		}
+						]).then((search)=> {
+							createTable(save, search.hashtag, true);
+						})
+					})
 		}
 		else if (answer.choice == "Exporter les données en BDD (excel)") {
-			console.log(2);
-		}
-		else {
-			askQuestion();
-		}
-	});
-}
-
-function chooseDatabase()
-{
-		let array = [];
-		fs.readdir("./", (err, files) => {
-			if(err)
-			{
-				console.error(err);
-			}
-			else
-			{
-				files
-         .forEach(function(index){
-					 if(~index.indexOf(".db")) {
-						 array.push(index);
-					 }
-				 });
-				 inquirer.prompt([
-			 		{
-			 			type: 'list',
-			 			message: 'Dans quelle base souhaitez vous enregistrer les données ?',
-			 			name: 'database',
-			 			choices: array
-			 		}
-			 	]).then((save) => {
-					inquirer.prompt([
- 			 		{
- 			 			type: 'input',
- 			 			message: 'Tapez le hashtag à rechercher',
- 			 			name: 'hashtag',
- 			 		}
-				]).then((answer)=> {
-					createTable(save.database, answer.hashtag);
-				})
+			askQuestion("De quelle base voulez-vous extraire les données ?").then((response)=>{
+				generateExcel(response);
 			})
 		}
-	})
+		else {
+			askQuestion("De quelle base voulez-vous voir les données ?").then((response)=>{
+				startServer(response);
+			})
+		}
+	});
 }
 
 function getTwitter(database, word)
@@ -104,13 +100,18 @@ function getTwitter(database, word)
 			 insertInDatabase(database, index.user.name, index.user.profile_image_url, index.text, word);
 		 })
 	});
+	console.log('Les données ont été correctement insérées');
+	menu();
 }
 
-function createTable(database, hashtag)
+function createTable(database, hashtag, insert)
 {
 	db.open(database).then(() => {
 		db.run('CREATE TABLE IF NOT EXISTS twitter (id INTEGER PRIMARY KEY, pseudo TEXT, description TEXT, picture TEXT, recherche TEXT)').then(()=>{
-			getTwitter(database, hashtag);
+			if(insert == true)
+			{
+				getTwitter(database, hashtag);
+			}
 		})
 	})
 }
@@ -141,33 +142,117 @@ function startServer(database)
 	spawn('open', ['http://localhost:8080']);
 }
 
-function askQuestion()
+function generateExcel(database)
 {
-	let array = [];
-	fs.readdir("./", (err, files) => {
-		if(err)
-		{
-			console.error(err);
-		}
-		else
-		{
-			files
-			 .forEach(function(index){
-				 if(~index.indexOf(".db")) {
-					 array.push(index);
-				 }
-			 });
-			 inquirer.prompt([
-				{
-					type: 'list',
-					message: 'De quelle base voulez-vous voir les données ?',
-					name: 'database',
-					choices: array
-				}
-			]).then((save) => {
-				startServer(save.database);
+	db.open(database).then(() => {
+		retrieveData().then((data) => {
+			let bigArray = [];
+			bigArray.push(["Id", "Pseudo", "Description", "Picture", "Recherche"])
+			data.forEach(function(index)
+			{
+				var array = [];
+				array.push(index.id);
+				array.push(index.pseudo);
+				array.push(index.description);
+				array.push(index.picture);
+				array.push(index.recherche);
+				bigArray.push(array);
 			})
+
+			var data = bigArray;
+			var ws_name = "Twitter Crawler";
+			var wb = new Workbook(), ws = sheet_from_array_of_arrays(data);
+			wb.SheetNames.push(ws_name);
+			wb.Sheets[ws_name] = ws;
+			xlsx.writeFile(wb, 'twitter.xlsx');
+			spawn('open', ['twitter.xlsx']);
+		})
+	})
+
+}
+
+function datenum(v, date1904) {
+	if(date1904) v+=1462;
+	var epoch = Date.parse(v);
+	return (epoch - new Date(Date.UTC(1899, 11, 30))) / (24 * 60 * 60 * 1000);
+}
+
+function sheet_from_array_of_arrays(data, opts) {
+	var ws = {};
+	var range = {s: {c:10000000, r:10000000}, e: {c:0, r:0 }};
+	for(var R = 0; R != data.length; ++R) {
+		for(var C = 0; C != data[R].length; ++C) {
+			if(range.s.r > R) range.s.r = R;
+			if(range.s.c > C) range.s.c = C;
+			if(range.e.r < R) range.e.r = R;
+			if(range.e.c < C) range.e.c = C;
+			var cell = {v: data[R][C] };
+			if(cell.v == null) continue;
+			var cell_ref = xlsx.utils.encode_cell({c:C,r:R});
+
+			if(typeof cell.v === 'number') cell.t = 'n';
+			else if(typeof cell.v === 'boolean') cell.t = 'b';
+			else if(cell.v instanceof Date) {
+				cell.t = 'n'; cell.z = xlsx.SSF._table[14];
+				cell.v = datenum(cell.v);
+			}
+			else cell.t = 's';
+
+			ws[cell_ref] = cell;
 		}
+	}
+	if(range.s.c < 10000000) ws['!ref'] = xlsx.utils.encode_range(range);
+	return ws;
+}
+
+function Workbook() {
+	if(!(this instanceof Workbook)) return new Workbook();
+	this.SheetNames = [];
+	this.Sheets = {};
+}
+
+function askQuestion(request)
+{
+	return new Promise(function(resolve,reject){
+		let array = [];
+		fs.readdir("./", (err, files) => {
+			if(err)
+			{
+				console.error(err);
+			}
+			else
+			{
+				files
+				 .forEach(function(index){
+					 if(~index.indexOf(".db")) {
+						 array.push(index);
+					 }
+				 });
+				 if(array.length > 0)
+				 {
+					 inquirer.prompt([
+						{
+							type: 'list',
+							message: request,
+							name: 'database',
+							choices: array
+						}
+					]).then((save) => {
+						resolve(save.database);
+					})
+				 }
+				 else {
+					 console.log("Veuillez commencer par créer une base en utilisant twitter -d [nom de la base]");
+				 	 process.exit(1);
+				 }
+			}
+		})
 	})
 }
+
+function retrieveData()
+{
+		return db.all('SELECT * FROM twitter');
+}
+
 start();
